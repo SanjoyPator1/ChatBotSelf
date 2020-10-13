81ea00619135bb51317fbb2ad91f926908d0b681
SanjoyPator <sanjoypator1@gmail.com>
2020-10-13 20:34:13 +0530

    final updates for now Ava, be happy

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from tensorflow.keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
 import random
-intents = json.loads(open('intents.json').read())
+intents = json.loads(open('intents.json', encoding="utf8").read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 
